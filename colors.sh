8368c344773e0e57f7111ef8fda7e7fd56316279
gabe <gabe@ninjacoden.com>
2019-07-10 22:41:24 -0700

    removed whitespae from first line

--- colors.sh
+++ colors.sh
@@ -1,4 +1,4 @@
-#!/bin/bash                                                                                                                                                                      
+#!/bin/bash
 # Bash Menu Script Example
 #for i in {0..15}; do printf "\x1b[38;5;${i}m███ color%-5i\x1b[0m" $i ; if ! (( ($i + 1 ) % 8 )); then echo ; fi ; done
 #for i in {0..15}; do printf "\x1b[38;5;${i}m███\x1b[0m \x1b[1;37;mColor%-5i\x1b[0m" $i ; if ! (( ($i + 1 ) % 8 )); then echo ; fi ; done
